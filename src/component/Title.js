a578dd700a7c772b4584f03b53cc911be537fda5
huytocdo <ndhuyspkt@gmail.com>
2018-10-02 11:37:04 +0700

    config sass for styling

---
+++ src/component/Title.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Title.scss';
+
+export default class Title extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="title-1 title-1-blue">
+          Hello World
+        </h1>
+      </div>
+    )
+  }
+}
